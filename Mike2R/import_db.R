7940b1b8f47efd570468bb6837ee7729743de814
Nick Baumann <nick.baumann@hest.ethz.ch>
2020-09-24 19:32:14 +0200

    cleaned up the r code a bit, added some comments, added generic pdfplot function

--- Mike2R/import_db.R
+++ Mike2R/import_db.R
@@ -86,69 +86,68 @@ plot_srd_arrows <- function(srd, bigger_is_better, height, data_x, data_y) {
 
 ## PLOTTING FUNCTIONS
 
+# Low level metric line plotter
 plot_metric_for_all_series <- function(title, metric, series_data, series_names, series_srd_names, x_col = 'IthSession') {
   m_info = metric_info[metric_info$Name == metric,]
   pretty_metric_name <- get_pretty_name(metric)
   pretty_name_with_unit <- paste(pretty_metric_name, get_unit(m_info))
-  
-  n <- nrow(series_names)
+
+  n <- length(series_names)
   palette(get_palette(n))
-  
+
   all_x <- do.call(ccat, lapply(series_data, function(data) data[[x_col]]))
   all_y <- do.call(ccat, lapply(series_data, function(data) data[[metric]]))
-  
+
   max_x <- max(all_x, na.rm = TRUE)
-  min_y <- min(all_y, na.rm = TRUE)
-  max_y <- max(all_y, na.rm = TRUE)
+  min_y <- min(all_y[is.finite(all_y)], na.rm = TRUE)
+  max_y <- max(all_y[is.finite(all_y)], na.rm = TRUE)
   y_vals <- range(min_y, max_y)
   if (!m_info$BiggerIsBetter) {
     y_vals <- rev(y_vals)
   }
-  
-  par(mar=c(5.1, 6.1, 4.1, 13.1))
+
+  legend_len = max(sapply(series_names, nchar))
+
+  par(mar=c(5.1, 6.1, 4.1, 8 + legend_len * 0.3))
   matplot(all_x, all_y, type = "b", pch=1, lwd=2, ylim = y_vals, lty = 1:n, col=1:n,
-          main=paste(title, pretty_metric_name), xlab="Session Nr", ylab=pretty_name_with_unit)
-  legend('bottomright',inset=c(-0.2,0), legend = do.call(c, series_names), col=1:n, lty = 1:n, lwd = 2, xpd=TRUE)
-  
+          main=title, xlab="Session Nr", ylab=pretty_name_with_unit)
+  legend('bottomright',inset=c(-0.15 - legend_len*0.012,0),
+         legend = do.call(c, series_names), col=1:n, lty = 1:n, lwd = 2, xpd=TRUE)
+
   healthy_avg = m_info$HealthyAvg
   if (!is.na(healthy_avg)) {
     abline(h=healthy_avg, col="darkgreen", lwd=2, lty=1)
     text(max_x, healthy_avg, "healthy avg \U2191", col = "darkgreen", xpd=TRUE, adj=-0.5)
   }
-  
-  mapply(function(data, srd_name) plot_srd_arrows(m_info[[srd_name]], m_info$BiggerIsBetter, max_y - min_y, data[[x_col]], data[[metric]]), series_data, series_srd_names)
+  par(mar=c(5.0, 5.0, 5.0, 5.0))
+
+  mapply(function(data, srd_name) plot_srd_arrows(m_info[[srd_name]], m_info$BiggerIsBetter, max_y - min_y, data[[x_col]], data[[metric]]),
+         series_data, series_srd_names)
 }
 
+
+# High level function to plot longitudinal data for a single user
 long_plot_metric <- function(user, metric) {
-  m_info = metric_info[metric_info$Name == metric,]
-  pretty_metric_name <- get_pretty_name(metric)
-  pretty_name_with_unit <- paste(pretty_metric_name, get_unit(m_info))
-  
-  imp <- impaired_data[impaired_data$SubjectNr == user,] # Trailing comma is important
-  nimp <- nonimpaired_data[nonimpaired_data$SubjectNr == user,]
-  
-  x <- ccat(imp$robotic_session_number, nimp$robotic_session_number)
-  y <- ccat(imp[[metric]], nimp[[metric]])
-  max_x <- max(x, na.rm = TRUE)
-  
-  matplot(x, y, type = "b", pch=1, lwd=2,
-          main=paste("Longitudinal Plot for", user), xlab="Session Nr", ylab=pretty_name_with_unit)
-  legend('topright', legend = c("impaired", "non-impaired"), col=1:2, lty = 1:2, lwd = 1)
-  
-  healthy_avg <- m_info$HealthyAvg
-  if (!is.na(healthy_avg)) {
-    abline(h=healthy_avg, col="darkgreen", lwd=2)
-    text(max_x, healthy_avg*1.05, "healthy avg", col = "darkgreen", adj=1)
-  }
+  plot_metric_for_all_series(paste("Longitudinal Plot for:", user),
+                             metric,
+                             list(impaired_data[impaired_data$SubjectNr == user,], nonimpaired_data[nonimpaired_data$SubjectNr == user,]),
+                             list("impaired", "non-impaired"),
+                             list("SrdImpaired", "SrdNonImpaired"), x_col = "robotic_session_number")
 }
 
+# High level function to plot longitudinal impaired data for all users
 all_user_long_plot_metric <- function(metric) {
   by_user <- impaired_data[!is.na(impaired_data$robotic_session_number),] %>% group_by(SubjectNr)
   data_per_user <- group_split(by_user)
   users <- group_keys(by_user)
-  plot_metric_for_all_series("Impaired hand:", metric, data_per_user, users, as.list(rep('SrdImpaired', nrow(users))))
+  plot_metric_for_all_series(paste("Impaired hand:", get_pretty_name(metric)),
+                             metric,
+                             data_per_user,
+                             as.list(t(users)),
+                             as.list(rep('SrdImpaired', nrow(users))))
 }
 
+# Box plot for a metric, comparing non impaired and impaired data
 box_plot_metric <- function(metric) {
   m_info = metric_info[metric_info$Name == metric,]
   pretty_metric_name <- get_pretty_name(metric)
@@ -161,17 +160,29 @@ box_plot_metric <- function(metric) {
           ylab=pretty_name_with_unit)
 }
 
-plot_all_metrics_with_healthy_avg <- function(filename) {
-  if (!missing(filename)) {
-    cairo_pdf(paste(filename, ".pdf", sep=""), width = 12, height=7, onefile = TRUE)
-  }
+# plot all metrics which have a healthy avg value defined (currently only front-end metrics)
+plot_all_metrics_with_healthy_avg <- function() {
   infos = metric_info[!is.na(metric_info$HealthyAvg),]
   for (i in rownames(infos)) {
     all_user_long_plot_metric(infos[i, 'Name'])
   }
-  if (!missing(filename)) {
-    dev.off()
+}
+
+# plot all metrics
+plot_all_metrics <- function() {
+  for (i in rownames(metric_info)) {
+    all_user_long_plot_metric(metric_info[i, 'Name'])
   }
+}
+
+# Run the specified plotting function and store result as pdf
+# Usage e.g. pdfplot("somefilename", plot_all_metrics)
+#       or   pdfplot("somefilename", plot_all_metrics_with_healthy_avg)  # -> to reproduce pdf with plots for all frontend metrics
+#       or   pdfplot("somefilename", function() all_user_long_plot_metric("Force_Flexion_MaxForce_Mean") )
+pdfplot <- function(filename, plot_function) {
+  cairo_pdf(paste(filename, ".pdf", sep=""), width = 10, height=6, onefile = TRUE)
+  plot_function()
+  dev.off()
 }
 
 # example plots
