04369d27f227dcaeb229e4e8af81e9fbb810ffa4
Nick Baumann <nick.baumann@hest.ethz.ch>
2020-10-05 09:43:10 +0200

    use path relative to python files for config file instead of relative to cwd

--- mike_analysis/cfg.py
+++ mike_analysis/cfg.py
@@ -1,15 +1,18 @@
 import os
 import importlib.util
+from pathlib import Path
 
 from mike_analysis.core.meta import Modes
 
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'config.py.template')) as file:
+__this_folder = Path(os.path.dirname(os.path.realpath(__file__)))
+with open(os.path.join(__this_folder, 'config.py.template')) as file:
     _TEMPLATE = file.read().replace("'$ALL_MODES'", f', '.join([f"'{mode.name}'" for mode in Modes])).lstrip()
 
-if not os.path.exists('config.py'):
-    with open('config.py', 'w') as fout:
+__config_file_path = __this_folder.parent.joinpath('config.py')
+if not __config_file_path.exists():
+    with open(__config_file_path, 'w') as fout:
         fout.write(_TEMPLATE)
 
-_spec = importlib.util.spec_from_file_location("config", './config.py')
+_spec = importlib.util.spec_from_file_location("config", __config_file_path)
 config = importlib.util.module_from_spec(_spec)
 _spec.loader.exec_module(config)
