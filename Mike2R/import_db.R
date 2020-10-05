cf1f7dbd9f871527dbc58b0ac2b5964954a1118e
Nick Baumann <nick.baumann@hest.ethz.ch>
2020-10-05 13:18:53 +0200

    improved R code style

--- Mike2R/import_db.R
+++ Mike2R/import_db.R
@@ -8,52 +8,52 @@ get_palette <- colorRampPalette(brewer.pal(5, "Set1"))
 palette(get_palette(8))
 
 # connect to db
-con <- dbConnect(drv=RSQLite::SQLite(), dbname="..\\analysis_db.db")
+con <- dbConnect(drv = RSQLite::SQLite(), dbname = "..\\analysis_db.db")
 
 ## Import data from database
 
 # Metric metadata
-metric_info <- dbGetQuery(conn=con, "SELECT *
-                                     FROM MetricInfo")
+metric_info <- dbGetQuery(conn = con, "SELECT *
+                                       FROM MetricInfo")
 numtrial_metrics <- metric_info[endsWith(metric_info$Name, "NumTrials"),]
 
 # Patient metadata (joined with demographics from redcap)
-patients <- dbGetQuery(conn=con, "SELECT *
-                                  FROM Patient AS P
-                                  JOIN Demographics AS D ON(P.SubjectNr == D.subject_code)")
+patients <- dbGetQuery(conn = con, "SELECT *
+                                    FROM Patient AS P
+                                    JOIN Demographics AS D ON(P.SubjectNr == D.subject_code)")
 
 # Metrics for single task
-force_metrics <- dbGetQuery(conn=con, "SELECT *
-                                       FROM ForceResultFull")
+force_metrics <- dbGetQuery(conn = con, "SELECT *
+                                         FROM ForceResultFull")
 # "", pre-joined with patient data
-force_metrics_w_patient <- dbGetQuery(conn=con, "SELECT *
-                                                 FROM ForceResultFull
-                                                 JOIN Patient AS P USING(PatientId)
-                                                 JOIN Demographics AS D ON(P.SubjectNr == D.subject_code)")
+force_metrics_w_patient <- dbGetQuery(conn = con, "SELECT *
+                                                   FROM ForceResultFull
+                                                   JOIN Patient AS P USING(PatientId)
+                                                   JOIN Demographics AS D ON(P.SubjectNr == D.subject_code)")
 # Perform a join in R instead of SQL
 force_metrics_w_patient_v2 <- merge(force_metrics, patients, by = "PatientId")
 
 # All Metrics pre-joined with patient data
-session_results <- dbGetQuery(conn=con, "SELECT *
-                                         FROM SessionResult")
+session_results <- dbGetQuery(conn = con, "SELECT *
+                                           FROM SessionResult")
 
 # All data (patient metadata + metrics + redcap data, ordered by patient name, left hand, ith_session)
-all_data <- dbGetQuery(conn=con, "SELECT *
-                                  FROM DataFull")
-impaired_data <- dbGetQuery(conn=con, "SELECT *
-                                       FROM DataImpaired")
-nonimpaired_data <- dbGetQuery(conn=con, "SELECT *
-                                          FROM DataNonImpaired")
+all_data <- dbGetQuery(conn = con, "SELECT *
+                                    FROM DataFull")
+impaired_data <- dbGetQuery(conn = con, "SELECT *
+                                         FROM DataImpaired")
+nonimpaired_data <- dbGetQuery(conn = con, "SELECT *
+                                            FROM DataNonImpaired")
 
 dbDisconnect(con)
 
 ## HELPER FUNCTIONS
 
-ccat = function(...) {
+ccat <- function(...) {
   vecs <- list(...)
   n <- max(sapply(vecs, length))
-  vecs <- lapply(vecs, function(vec) {length(vec) <- n;vec})
-  res <- matrix(unlist(vecs), ncol=length(vecs))
+  vecs <- lapply(vecs, function(vec) { length(vec) <- n; vec })
+  res <- matrix(unlist(vecs), ncol = length(vecs))
   return(res)
 }
 
@@ -63,25 +63,25 @@ get_pretty_name <- function(metric) {
 
 get_unit <- function(metric_info) {
   unit <- metric_info$Unit
-  if (unit != '') unit <- paste("[", unit, "]", sep="")
+  if (unit != '') unit <- paste0("[", unit, "]")
   return(unit)
 }
 
 plot_srd_arrows <- function(srd, bigger_is_better, height, data_x, data_y) {
   if (length(data_x) == 1 || is.na(srd)) return()
 
-  diff_data = diff(data_y)
-  for (i in 1:length(diff_data)) {
+  diff_data <- diff(data_y)
+  for (i in seq_along(diff_data)) {
     dy <- diff_data[i]
-    x <- (data_x[i+1] + data_x[i]) / 2.0
-    y_cent <- (data_y[i+1] + data_y[i]) / 2.0
+    x <- (data_x[i + 1] + data_x[i]) / 2.0
+    y_cent <- (data_y[i + 1] + data_y[i]) / 2.0
     y_offset <- height * 0.04
     arrow_col <- 'gray42' #if ((dy > srd && bigger_is_better) || (dy < -srd && !bigger_is_better)) 'springgreen3' else 'red'
     if (dy > srd) {
-      arrows(x, y_cent - y_offset * 0.5, x, y_cent + y_offset, col=arrow_col, lwd=2.5, length=0.10)
+      arrows(x, y_cent - y_offset * 0.5, x, y_cent + y_offset, col = arrow_col, lwd = 2.5, length = 0.10)
     }
     else if (dy < -srd) {
-      arrows(x, y_cent+y_offset * 0.5, x, y_cent - y_offset, col=arrow_col, lwd=2.5, length=0.10)
+      arrows(x, y_cent + y_offset * 0.5, x, y_cent - y_offset, col = arrow_col, lwd = 2.5, length = 0.10)
     }
   }
 }
@@ -103,40 +103,40 @@ plot_metric_for_all_series <- function(title, metric, series_data, series_names,
   max_x <- max(all_x, na.rm = TRUE)
   min_y <- min(all_y[is.finite(all_y)], na.rm = TRUE)
   max_y <- max(all_y[is.finite(all_y)], na.rm = TRUE)
-  delta_y = max_y - min_y
+  delta_y <- max_y - min_y
   y_vals <- range(min_y - delta_y * 0.05, max_y + delta_y * 0.05)
   if (!m_info$BiggerIsBetter) {
     y_vals <- rev(y_vals)
   }
 
-  legend_len = max(sapply(series_names, nchar))
+  legend_len <- max(sapply(series_names, nchar))
 
-  par(mar=c(5.1, 6.1, 4.1, 8 + legend_len * 0.3))
-  matplot(all_x, all_y, type = "b", pch=1, lwd=2, ylim = y_vals, lty = 1:n, col=1:n,
-          main=title, xlab="Session Nr", ylab=pretty_name_with_unit)
-  legend('bottomright',inset=c(-0.15 - legend_len*0.012,0),
-         legend = do.call(c, series_names), col=1:n, lty = 1:n, lwd = 2, xpd=TRUE)
+  par(mar = c(5.1, 6.1, 4.1, 8 + legend_len * 0.3))
+  matplot(all_x, all_y, type = "b", pch = 1, lwd = 2, ylim = y_vals, lty = 1:n, col = 1:n,
+          main = title, xlab = "Session Nr", ylab = pretty_name_with_unit)
+  legend('bottomright', inset = c(-0.15 - legend_len * 0.012, 0),
+         legend = do.call(c, series_names), col = 1:n, lty = 1:n, lwd = 2, xpd = TRUE)
 
-  healthy_avg = m_info$HealthyAvg
+  healthy_avg <- m_info$HealthyAvg
   if (!is.na(healthy_avg)) {
-    abline(h=healthy_avg, col="darkgreen", lwd=2, lty=1)
-    text(max_x, healthy_avg, "healthy avg \U2191", col = "darkgreen", xpd=TRUE, adj=-0.5)
+    abline(h = healthy_avg, col = "darkgreen", lwd = 2, lty = 1)
+    text(max_x, healthy_avg, "healthy avg \U2191", col = "darkgreen", xpd = TRUE, adj = -0.5)
   }
-  par(mar=c(5.0, 5.0, 5.0, 5.0))
+  par(mar = c(5.0, 5.0, 5.0, 5.0))
 
   mapply(function(data, srd_name) plot_srd_arrows(m_info[[srd_name]], m_info$BiggerIsBetter, max_y - min_y, data[[x_col]], data[[metric]]),
          series_data, series_srd_names)
 
   # Add std deviation bars for mean metrics
   if (show_std && endsWith(metric, "Mean")) {
-    std_metric <- paste(substr(metric, 0, nchar(metric) - 4), "Std", sep="")
+    std_metric <- paste0(substr(metric, 0, nchar(metric) - 4), "Std")
     if (std_metric %in% colnames(series_data[[1]])) {
       # count_metric_name = numtrial_metrics[numtrial_metrics$TaskType == m_info$TaskType
       #                                      & startsWith(metric, substr(numtrial_metrics$Name, 1, nchar(numtrial_metrics$Name) - 9)),]$Name
       for (series in series_data) {
         sdev <- series[[std_metric]]
         #sem = series[[std_metric]] / sqrt(series[[count_metric_name]])
-        arrows(series[[x_col]], series[[metric]]-sdev, series[[x_col]], series[[metric]]+sdev, length=0.05, angle=90, code=3, xpd=TRUE)
+        arrows(series[[x_col]], series[[metric]] - sdev, series[[x_col]], series[[metric]] + sdev, length = 0.05, angle = 90, code = 3, xpd = TRUE)
       }
     }
   }
@@ -173,9 +173,9 @@ box_plot_metric <- function(metric) {
 
   boxplot(impaired_data[impaired_data$IthSession == 1,][[metric]],
           nonimpaired_data[nonimpaired_data$IthSession == 1,][[metric]],
-          main=paste("Compare", pretty_metric_name, "distributions"),
+          main = paste("Compare", pretty_metric_name, "distributions"),
           names = c("impaired", "non-impaired"),
-          ylab=pretty_name_with_unit)
+          ylab = pretty_name_with_unit)
 }
 
 # plot all metrics which have a healthy avg value defined (currently only front-end metrics)
@@ -194,27 +194,27 @@ plot_all_metrics <- function() {
 }
 
 # recommended pdf dims for all complete metrics width=20, height=20
-plot_correlation_summary <- function(impaired=TRUE, only_first_trial=TRUE) {
+plot_correlation_summary <- function(impaired = TRUE, only_first_trial = TRUE) {
   df <- if (impaired) impaired_data else nonimpaired_data
 
   # Remove patients without redcap data
   df <- df[!is.na(df$date_of_demographics),]
 
   if (only_first_trial) {
-    df <- df[df$IthSession==1,]
+    df <- df[df$IthSession == 1,]
   }
 
   # Remove non-numeric columns
   df <- select_if(df, is.numeric)
 
   # Replace inf values by NA
-  df <- do.call(data.frame,lapply(df, function(x) replace(x, is.infinite(x),NA)))
+  df <- do.call(data.frame, lapply(df, function(x) replace(x, is.infinite(x), NA)))
 
   # Remove columns with 0 variance or containing na values
   df <- Filter(function(df) var(df), df)
 
   M <- cor(df)
-  corrplot(M, type="upper", order="hclust", col=brewer.pal(n=8, name="RdYlBu"))
+  corrplot(M, type = "upper", order = "hclust", col = brewer.pal(n = 8, name = "RdYlBu"))
 }
 
 # Run the specified plotting function and store result as pdf
@@ -222,8 +222,8 @@ plot_correlation_summary <- function(impaired=TRUE, only_first_trial=TRUE) {
 #       or   pdfplot("somefilename", plot_all_metrics_with_healthy_avg)  # -> to reproduce pdf with plots for all frontend metrics
 #       or   pdfplot("somefilename", function() all_user_long_plot_metric("Force_Flexion_MaxForce_Mean") )
 #       or   pdfplot("somefilename", plot_correlation_summary, width=20, height=20)
-pdfplot <- function(filename, plot_function, width=10, height=6) {
-  cairo_pdf(paste(filename, ".pdf", sep=""), width=width, height=height, onefile = TRUE)
+pdfplot <- function(filename, plot_function, width = 10, height = 6) {
+  cairo_pdf(paste0(filename, ".pdf"), width = width, height = height, onefile = TRUE)
   plot_function()
   dev.off()
 }
