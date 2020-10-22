7403d509229b122a43b79cdc653198865a6b8baa
Nick Baumann <nick.baumann@hest.ethz.ch>
2020-10-22 10:55:24 +0200

    set mapr for non-moving patients to 1.0

--- mike_analysis/metrics/motor.py
+++ mike_analysis/metrics/motor.py
@@ -45,6 +45,8 @@ class MAPR(TrialMetric):
 
     def compute_single_trial(self, trial_data: pd.DataFrame, precomputed: PrecomputeDict, db_trial_result: RowDict) -> Scalar:
         abs_vel = precomputed[AbsVelocity]
+        if sum(abs_vel < 0.05) > 0.9 * len(abs_vel):
+            return 1.0
         v_thresh = 0.2 * abs_vel.mean()
         mapr = (abs_vel < v_thresh).sum() / float(len(trial_data))
         return mapr
