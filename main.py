e1eb38b8423daa8f3c69b523e67bcfbd74f2c4cd
Manumerous <manuel.galliker@gmx.ch>
2022-02-16 14:09:11 +0100

    move main file to top level of package

--- main.py
+++ main.py
@@ -20,13 +20,16 @@ def import_and_process_everything(db_path: str, polybox_upload_dir: str, data_di
 
     # Open database files
     try:
-        in_conn = sqlite3.connect(f'{pathlib.Path(db_path).absolute().as_uri()}?mode=ro', uri=True) # Open db.db in read-only mode
+        # Open db.db in read-only mode
+        in_conn = sqlite3.connect(
+            f'{pathlib.Path(db_path).absolute().as_uri()}?mode=ro', uri=True)
         out_conn = sqlite3.connect('analysis_db.db')
     except Exception as e:
         print(f'ERROR: Failed to open db\n{e}')
         sys.exit(-1)
 
     try:
+        # migrates data from input table (mike front end db) to analysis db
         migrator = TableMigrator(in_conn, out_conn)
 
         # Copy patient, session and completed assessment data from input database
@@ -38,16 +41,19 @@ def import_and_process_everything(db_path: str, polybox_upload_dir: str, data_di
         migrator.migrate_table_index_or_view(f'{Tables.Session}_PatientId')
         migrator.migrate_table_data(Tables.Session)
 
-        migrator.migrate_table_index_or_view(f'{Tables.Assessment}', overwrite=True)
+        migrator.migrate_table_index_or_view(
+            f'{Tables.Assessment}', overwrite=True)
         migrator.migrate_table_index_or_view(f'{Tables.Assessment}_SessionId')
-        migrator.migrate_table_data(Tables.Assessment, f'State == {AssessmentState.Finished} AND IsTrialRun IS FALSE')
+        migrator.migrate_table_data(
+            Tables.Assessment, f'State == {AssessmentState.Finished} AND IsTrialRun IS FALSE')
 
         # Import data from redcap if enabled
         if cfg.REDCAP_IMPORT:
             try:
                 RedcapImporter(migrator, out_conn).import_all_from_redcap()
             except Exception as e:
-                print(f'There was a problem while importing data from redcap:\n{e}')
+                print(
+                    f'There was a problem while importing data from redcap:\n{e}')
                 sys.exit(-2)
 
         # Import all data, compute metrics and store results in analysis database
@@ -70,15 +76,18 @@ def main(args):
     elif cfg.DB_PATH:
         db_path = cfg.DB_PATH
     elif cfg.USE_DB_FROM_UPLOAD_DIR and os.path.exists(cfg.PATH_TO_POLYBOX_UPLOAD_DIR):
-        db_paths = os.listdir(os.path.join(cfg.PATH_TO_POLYBOX_UPLOAD_DIR, 'Database Backups'))
+        db_paths = os.listdir(os.path.join(
+            cfg.PATH_TO_POLYBOX_UPLOAD_DIR, 'Database Backups'))
         if db_paths:
-            db_path = os.path.join(cfg.PATH_TO_POLYBOX_UPLOAD_DIR, 'Database Backups', max(db_paths))
+            db_path = os.path.join(
+                cfg.PATH_TO_POLYBOX_UPLOAD_DIR, 'Database Backups', max(db_paths))
         else:
             db_path = 'db.db'
     else:
         db_path = 'db.db'
 
-    import_and_process_everything(db_path, cfg.PATH_TO_POLYBOX_UPLOAD_DIR, cfg.PATH_TO_DATA_ROOT_DIR)
+    import_and_process_everything(
+        db_path, cfg.PATH_TO_POLYBOX_UPLOAD_DIR, cfg.PATH_TO_DATA_ROOT_DIR)
     # profiler.stop()
     # print(profiler.output_text(unicode=True, color=True))
 
