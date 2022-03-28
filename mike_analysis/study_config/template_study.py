ea8491405987df3c1337e8870ca1d758f7357805
Manumerous <manuel.galliker@gmx.ch>
2022-03-28 14:18:40 +0200

    Adapting default config

--- mike_analysis/study_config/template_study.py
+++ mike_analysis/study_config/template_study.py
@@ -1,6 +1,33 @@
 from mike_analysis.core.constants import RCCols
 from mike_analysis.core.sqlite_migrator import SQLiteMigrator
 
+###########################################################
+# Tables
+############################################################
+
+# Can be used to copy e.g. tables needed to copy certain views
+# always specify "table_name: table_index"
+IMPORT_TABLES = {}
+
+IMPORT_ASSESSMENT_TABLES = {}
+IMPORT_THERAPY_TABLES = {}
+
+############################################################
+# Views
+############################################################
+
+# Copy over the assesment result views. make sure that the tables the views depend on are specified in IMPORT_ADDITIONAL_TABLES.
+
+# Views with one row per trial
+IMPORT_ASSESSMENT_RESULTS_FULL_VIEW = False
+# Views with average score over all trials of the same exercise
+IMPORT_ASSESSMENT_RESULTS_AGGREGATE_VIEW = False
+
+# Copy over the therapy result views:
+# Views with one row per trial
+IMPORT_THERAPY_RESULTS_FULL_VIEW = False
+# Views with average score over all trials of the same exercise
+IMPORT_THERAPY_RESULTS_AGGREGATE_VIEW = False
 
 ############################################################
 # REDCap
